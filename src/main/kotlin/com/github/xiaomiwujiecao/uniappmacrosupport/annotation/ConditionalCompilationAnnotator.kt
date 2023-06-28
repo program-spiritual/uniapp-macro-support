857d74d7d2ecfd5c9c80de9aa11f2b022131fe40
hhy <huhongyun@ykccn.com>
2023-06-28 20:10:17 +0800

    refactor(context): 重构代码高亮部分

---
+++ src/main/kotlin/com/github/xiaomiwujiecao/uniappmacrosupport/annotation/ConditionalCompilationAnnotator.kt
@@ -0,0 +1,29 @@
+package com.github.xiaomiwujiecao.uniappmacrosupport.annotation
+
+import com.intellij.lang.annotation.AnnotationHolder
+import com.intellij.lang.annotation.Annotator
+import com.intellij.lang.annotation.HighlightSeverity
+import com.intellij.openapi.diagnostic.thisLogger
+import com.intellij.openapi.editor.colors.TextAttributesKey
+import com.intellij.psi.PsiComment
+import com.intellij.psi.PsiElement
+
+class ConditionalCompilationAnnotator : Annotator {
+    override fun annotate(element: PsiElement, holder: AnnotationHolder) {
+        thisLogger().debug("element:",element)
+        // 检查是否是注释
+        if (element is PsiComment) {
+            val text = element.text
+            thisLogger().debug("text:",text)
+            // 检查是否是条件编译标记
+            if (text.contains("#ifdef", true) || text.contains("#ifndef", true) || text.contains("#endif", true)) {
+                // 设置高亮显示的文本属性
+                val attributes = TextAttributesKey.createTextAttributesKey("CONDITIONAL_COMPILATION")
+                holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+                    .range(element)
+                    .textAttributes(attributes)
+                    .create()
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/com/github/xiaomiwujiecao/uniappmacrosupport/folding/ConditionalCompilationFoldingBuilder.kt
@@ -0,0 +1,44 @@
+package com.github.xiaomiwujiecao.uniappmacrosupport.folding
+import com.intellij.lang.ASTNode
+import com.intellij.lang.folding.FoldingBuilderEx
+import com.intellij.lang.folding.FoldingDescriptor
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiComment
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PsiTreeUtil
+class ConditionalCompilationFoldingBuilder : FoldingBuilderEx() {
+    override fun isCollapsedByDefault(node: ASTNode): Boolean {
+        return false
+    }
+
+    override fun buildFoldRegions(root: PsiElement, document: Document, quick: Boolean): Array<FoldingDescriptor> {
+        val descriptors = mutableListOf<FoldingDescriptor>()
+        val comments = PsiTreeUtil.findChildrenOfType(root, PsiComment::class.java)
+
+        var startOffset = -1
+        for (comment in comments) {
+            val text = comment.text
+            if (text.contains("#ifdef",true) || text.contains("#ifndef",true)) {
+                startOffset = comment.textRange.startOffset
+            } else if (text.contains("#endif")) {
+                if (startOffset != -1) {
+                    val endOffset = comment.textRange.endOffset
+                    val foldingRange = TextRange(startOffset, endOffset)
+                    descriptors.add(FoldingDescriptor(comment, foldingRange))
+                    startOffset = -1
+                }
+            }
+        }
+
+        return descriptors.toTypedArray()
+    }
+
+    override fun getPlaceholderText(node: ASTNode): String? {
+        return "..."
+    }
+
+    override fun getPlaceholderText(node: ASTNode, range: TextRange): String? {
+        return "..."
+    }
+}
