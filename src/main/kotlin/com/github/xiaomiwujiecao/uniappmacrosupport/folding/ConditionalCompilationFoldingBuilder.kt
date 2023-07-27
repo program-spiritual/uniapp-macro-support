ce64c5aa9ecc0ebe2f5bcf9891df62bc80fac8aa
xiaomiwujiecao <xiaomiwujiecao@gmail.com>
2023-07-28 01:09:16 +0800

    chore: performance improve

--- src/main/kotlin/com/github/xiaomiwujiecao/uniappmacrosupport/folding/ConditionalCompilationFoldingBuilder.kt
+++ src/main/kotlin/com/github/xiaomiwujiecao/uniappmacrosupport/folding/ConditionalCompilationFoldingBuilder.kt
@@ -1,4 +1,5 @@
 package com.github.xiaomiwujiecao.uniappmacrosupport.folding
+
 import com.intellij.lang.ASTNode
 import com.intellij.lang.folding.FoldingBuilderEx
 import com.intellij.lang.folding.FoldingDescriptor
@@ -7,38 +8,46 @@ import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiComment
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiTreeUtil
+
 class ConditionalCompilationFoldingBuilder : FoldingBuilderEx() {
     override fun buildFoldRegions(root: PsiElement, document: Document, quick: Boolean): Array<FoldingDescriptor> {
-        val descriptors = mutableListOf<FoldingDescriptor>()
-        val comments = PsiTreeUtil.findChildrenOfType(root, PsiComment::class.java)
+    val descriptors = mutableListOf<FoldingDescriptor>()
+    val comments = PsiTreeUtil.findChildrenOfType(root, PsiComment::class.java)
 
-        var startOffset = -1
-        for (comment in comments) {
-            val text = comment.text
-            if (text.contains("#ifdef", true) || text.contains("#ifndef", true)) {
-                startOffset = comment.textRange.startOffset
-            } else if (text.contains("#endif")) {
-                if (startOffset != -1) {
-                    val endOffset = comment.textRange.endOffset
-                    val foldingRange = TextRange(startOffset, endOffset)
-                    descriptors.add(FoldingDescriptor(comment, foldingRange))
-                    startOffset = -1
-                }
+    var startOffset = -1
+    val ifdefStack = mutableListOf<PsiComment>()
+
+    for (comment in comments) {
+        val text = comment.text
+        if (text.contains("#ifdef", true) || text.contains("#ifndef", true)) {
+            startOffset = comment.textRange.startOffset
+            ifdefStack.add(comment)
+        } else if (text.contains("#endif")) {
+            if (startOffset != -1 && ifdefStack.isNotEmpty()) {
+                val endOffset = comment.textRange.endOffset
+                val foldingRange = TextRange(startOffset, endOffset)
+                descriptors.add(FoldingDescriptor(comment, foldingRange))
+                startOffset = -1
+                ifdefStack.removeLast()
             }
         }
-
-        return descriptors.toTypedArray()
     }
 
-    override fun getPlaceholderText(node: ASTNode): String? {
+    return descriptors.toTypedArray()
+}
+
+    override fun getPlaceholderText(node: ASTNode): String {
         return "..."
     }
 
     override fun isCollapsedByDefault(p0: ASTNode): Boolean {
-        return true
+        return false
     }
 
-    override fun getPlaceholderText(node: ASTNode, range: TextRange): String? {
+    override fun getPlaceholderText(node: ASTNode, range: TextRange): String {
         return "..."
     }
 }
+
+
+
