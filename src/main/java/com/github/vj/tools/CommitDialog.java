1574eb1f3c249884473b480519c9f33592b6636f
vj <vj@vjlanguage.com>
2023-09-18 14:52:07 +0800

    chore: git tools

---
+++ src/main/java/com/github/vj/tools/CommitDialog.java
@@ -0,0 +1,34 @@
+package com.github.vj.tools;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.DialogWrapper;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+/**
+ * @author Damien Arrachequesne
+ */
+public class CommitDialog extends DialogWrapper {
+
+    private final CommitPanel panel;
+
+    CommitDialog(@Nullable Project project, CommitMessage commitMessage) {
+        super(project);
+        panel = new CommitPanel(project, commitMessage);
+        setTitle("Commit");
+        setOKButtonText("OK");
+        init();
+    }
+
+    @Nullable
+    @Override
+    protected JComponent createCenterPanel() {
+        return panel.getMainPanel();
+    }
+
+    CommitMessage getCommitMessage() {
+        return panel.getCommitMessage();
+    }
+
+}
---
+++ src/main/java/com/github/vj/tools/CreateCommitAction.java
@@ -0,0 +1,51 @@
+package com.github.vj.tools;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.ui.DialogWrapper;
+import com.intellij.openapi.vcs.CheckinProjectPanel;
+import com.intellij.openapi.vcs.CommitMessageI;
+import com.intellij.openapi.vcs.VcsDataKeys;
+import com.intellij.openapi.vcs.ui.Refreshable;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * @author Damien Arrachequesne
+ */
+public class CreateCommitAction extends AnAction implements DumbAware {
+
+    @Override
+    public void actionPerformed(AnActionEvent actionEvent) {
+        CommitMessageI commitPanel = getCommitPanel(actionEvent);
+        if (commitPanel == null) return;
+
+        CommitMessage commitMessage = parseExistingCommitMessage(commitPanel);
+        CommitDialog dialog = new CommitDialog(actionEvent.getProject(), commitMessage);
+        dialog.show();
+
+        if (dialog.getExitCode() == DialogWrapper.OK_EXIT_CODE) {
+            commitPanel.setCommitMessage(dialog.getCommitMessage().toString());
+        }
+    }
+
+    private CommitMessage parseExistingCommitMessage(CommitMessageI commitPanel) {
+        if (commitPanel instanceof CheckinProjectPanel) {
+            String commitMessageString = ((CheckinProjectPanel) commitPanel).getCommitMessage();
+            return CommitMessage.parse(commitMessageString);
+        }
+        return null;
+    }
+
+    @Nullable
+    private static CommitMessageI getCommitPanel(@Nullable AnActionEvent e) {
+        if (e == null) {
+            return null;
+        }
+        Refreshable data = Refreshable.PANEL_KEY.getData(e.getDataContext());
+        if (data instanceof CommitMessageI) {
+            return (CommitMessageI) data;
+        }
+        return VcsDataKeys.COMMIT_MESSAGE_CONTROL.getData(e.getDataContext());
+    }
+}
